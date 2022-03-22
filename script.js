04af8ba2e49806cdbfb1908a644e74bd17ce02db
pavmys <pavlom2901@gmail.com>
2022-03-22 12:38:28 +0200

    added some js and css

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let letters = document.getElementsByClassName("letter");
+let outputWords = document.querySelector(".output-words");
 //console.log(letters[3].value);
 for (let i = 0; i < 33; i++) {
     letters[i].addEventListener("click", () => {
         console.log(letters[i].value);
     });
-
 }
 
-let words = ['']
+$(".words-h3").click(function () {
+    words.style.visibility = "visible";
+    $(".words").toggle("slow");
+});
+
+let words = document.querySelector(".words");
+let wordsArr = words.innerHTML.split(",");
+let lettersWordsArr = [];
+for (let j = 0; j < wordsArr.length; j++) {
+    lettersWordsArr.push(wordsArr[j].split(""));
+}
+console.log(lettersWordsArr);
